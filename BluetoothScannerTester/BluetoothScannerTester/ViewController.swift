ce31a344de6075c999999227311f073a9e737cb1
Marek Niedbach <marek.niedbach@silvair.com>
2019-11-06 15:08:36 +0100

    Initial commit

---
+++ BluetoothScannerTester/BluetoothScannerTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothScannerTester
+//
+//  Created by Marek Niedbach on 06/11/2019.
+//  Copyright © 2019 Silvair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
